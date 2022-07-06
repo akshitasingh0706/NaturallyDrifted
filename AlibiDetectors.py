13ef941ee61c556dbc75712b574ddda17d3db000
akshitasingh0706 <akshitasingh0706@gmail.com>
2022-07-06 01:15:36 -0400

    more updates with alibi detect

---
+++ AlibiDetectors.py
@@ -0,0 +1,112 @@
+import nlp
+import pandas as pd
+import numpy as np
+import os
+import tensorflow as tf
+from transformers import AutoTokenizer
+from functools import partial
+
+from alibi_detect.cd import KSDrift, MMDDrift, LearnedKernelDrift, ClassifierDrift, LSDDDrift
+from alibi_detect.utils.saving import save_detector, load_detector
+from alibi_detect.models.tensorflow import TransformerEmbedding
+from alibi_detect.cd.tensorflow import UAE
+from alibi_detect.cd.tensorflow import preprocess_drift
+
+from sampling import samplingData
+
+class AlibiDetectors:
+    def __init__(self, 
+                data_ref: Optional[Union[np.ndarray, list, None]],
+                data_h0: Optional[Union[np.ndarray, list, None]],
+                data_h1: Union[np.ndarray, list], # "data" in sample_data_gradual
+                test: Union["MMDDrift", "LSDDDrift", "LearnedKernel"],
+                sample_size: int, 
+                windows: Optional[int],
+                drift_type: Optional[Union["Sudden", "Gradual"]],
+                model_name: str,   
+                 
+                # emb_type: Optional[str],
+                emb_type: Optional[Union['pooler_output', 'last_hidden_state', 'hidden_state', 'hidden_state_cls']],
+                n_layers: Optional[int],
+                max_len: Optional[int],
+                enc_dim: Optional[int],
+                batch_size: Optional[int],
+                tokenizer_size: Optional[int] = 3 # keep it small else computer breaks down
+               
+                 ):
+        self.data_ref = data_ref
+        self.data_h0  = data_h0
+        self.data_h1  = data_h1
+        self.test = test
+        self.sample_size = sample_size
+        self.windows = windows
+        self.drift_type = drift_type
+        self.model_name = model_name
+
+        self.emb_type = emb_type
+        self.n_layers = n_layers
+        self.max_len = max_len
+        self.enc_dim = enc_dim
+        self.tokenizer_size = tokenizer_size
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.batch_size = batch_size
+
+    def preprocess(self):
+        layers = [-_ for _ in range(1, self.n_layers + 1)]
+
+        embedding = TransformerEmbedding(self.model_name, self.emb_type, layers)
+        tokens = self.tokenizer(list(X1[:self.tokenizer_size]), pad_to_max_length=True, 
+                                max_length= self.max_len, return_tensors='tf')
+        x_emb = embedding(tokens)
+        shape = (x_emb.shape[1],)
+        uae = UAE(input_layer=embedding, shape=shape, enc_dim= self.enc_dim)
+        return uae
+
+    def detector(self):
+        sample = samplingData(data_ref = self.data_ref, data_h0 = self.data_h0, data_h1 = self.data_h1, 
+                               drift_type = self.drift_type, sample_size = self.sample_size, windows = self.windows)
+        sample_dict = sample.samples()
+        data_ref = sample_dict[0]
+
+        uae = self.preprocess()
+        preprocess_fn = partial(preprocess_drift, model= uae, tokenizer= self.tokenizer, 
+                        max_len= self.max_len, batch_size= self.batch_size)
+        if self.test == "MMDDrift": 
+            cd = MMDDrift(data_ref, p_val=.05, preprocess_fn=preprocess_fn, 
+                      n_permutations=100, input_shape=(self.max_len,))
+        elif self.test == "LSDDDrift":
+            cd = LSDDDrift(data_ref, p_val=.05, preprocess_fn=preprocess_fn, 
+                      n_permutations=100, input_shape=(self.max_len,))
+        elif self.test == "LearnedKernel":
+            pass
+        else:
+            print("The following is not ")
+        return cd 
+    
+    def predict(self):
+        labels = ['No!', 'Yes!']
+        cd = self.detector()
+  
+        sample = samplingData(data_ref = self.data_ref, data_h0 = self.data_h0, data_h1 = self.data_h1, 
+                               drift_type = self.drift_type, sample_size = self.sample_size, windows = self.windows)
+        sample_dict = sample.samples()
+
+        if self.drift_type == "Sudden":  
+            for i, data_name in enumerate(["X_h0, X_comp"]):
+                data = sample_dict[i+1]
+                print("Drift results for ", data_name ,"data using ", self.test, "test:")
+                preds = cd.predict(data)
+                print('Drift? {}'.format(labels[preds['data']['is_drift']]))
+                print('p-value: {}'.format(preds['data']['p_val']))
+
+        elif self.drift_type == "Gradual":
+            if type(self.windows) is not int:
+                print("Please fill/correct windows parameter")
+            for ww in range(1,self.windows):
+                data = sample_dict[ww]
+                print("Drift results for window: ", ww, "data using", self.test, "test:")
+                preds = cd.predict(data)
+                print('Drift? {}'.format(labels[preds['data']['is_drift']]))
+                print('p-value: {}'.format(preds['data']['p_val']))
+        else:
+            print("The following drift type is not included")
---
+++ myDetectors.py
@@ -0,0 +1,173 @@
+from math import log2
+import numpy as np
+from scipy import stats
+import pandas as pd
+from typing import Callable, Dict, Optional, Union
+import matplotlib.pyplot as plt
+
+from sampling import samplingData
+from baseModels import baseModels
+from embedding import embedding
+
+class detectors:
+    def __init__(self, 
+                data_ref: Optional[Union[np.ndarray, list, None]],
+                data_h0: Optional[Union[np.ndarray, list, None]],
+                data_h1: Union[np.ndarray, list], # "data" in sample_data_gradual
+                test: Union["KS", "KL", "JS"],
+                sample_size: int = 500, 
+                windows: Optional[int] = 20,
+                drift_type: Optional[Union["Sudden", "Gradual"]] = "Sudden",
+                embedding_model: Union["Doc2Vec", "SBERT"] = "Doc2Vec",
+                model_name: Optional[Union[str, None]] = None, 
+                transformation: Union["PCA", "SVD", "UMAP", None] = None,
+                iters = 20):
+        self.data_ref = data_ref
+        self.data_h0  = data_h0
+        self.data_h1  = data_h1
+        self.test = test
+        self.sample_size = sample_size
+        self.windows = windows
+        self.drift_type = drift_type
+        self.embedding_model = embedding_model
+        self.model_name = model_name
+        self.transformation = transformation
+        self.iters = iters
+
+    def kl_divergence(self, p, q):
+        return sum(p[i] * log2(p[i]/q[i]) if p[i] > 0 and q[i] > 0 else 0 for i in range(len(p)))
+
+    def js_divergence(self, p, q):
+        return .5*(self.kl_divergence(p, q) + self.kl_divergence(q, p))
+
+    '''
+    function to calculate feature level tests:
+        KL Divergence
+        JS Divergence
+        KS Test
+    '''
+    def ks_doc2vec(self):
+        embs = embedding(data_ref = self.data_ref, data_h0 = self.data_h0, data_h1 = self.data_h1, test = self.test, 
+                        sample_size = self.sample_size, windows = self.windows, drift_type = self.drift_type, 
+                        embedding_model = self.embedding_model, model_name = self.model_name, 
+                        transformation = self.transformation, emb_iters = self.iters)
+        final_dict = embs.final_embeddings()  
+        iterations = len(final_dict)
+        windows = len(final_dict[0])
+        dists = np.zeros((iterations, windows - 1)) # distances or p-values for (ref, every other window)
+        pvals = np.zeros((iterations, windows - 1))
+        for it in range(iterations):
+            for ww in range(1, windows): 
+                dists[it, ww -1] = stats.ks_2samp(final_dict[it][0], final_dict[it][ww])[0] 
+                pvals[it, ww -1] = stats.ks_2samp(final_dict[it][0], final_dict[it][ww])[1] 
+
+        # plotting results
+        for window in range(pvals.shape[1]):
+          plt.plot(pvals[:, window])
+        plt.title("P-values for Doc2Vec + KS Test")
+        plt.legend(["ww-"+ str(i) for i in range(pvals.shape[1])])
+        return dists, pvals
+
+    def ks_sbert(self):
+        embs = embedding(data_ref = self.data_ref, data_h0 = self.data_h0, data_h1 = self.data_h1, test = self.test, 
+                        sample_size = self.sample_size, windows = self.windows, drift_type = self.drift_type, 
+                        model_name = self.model_name, transformation = self.transformation)
+        final_dict = embs.final_embeddings()  
+        windows = len(final_dict.keys())
+        dimensions = final_dict[0].shape[1]
+        dists = np.zeros((windows - 1, dimensions)) # distances or p-values for (ref, every other window)
+        pvals = np.zeros((windows - 1, dimensions))
+        for ww in range(1, windows): 
+            for dim in range(dimensions):
+                dists[ww -1, dim] = stats.ks_2samp(final_dict[0][:, dim], final_dict[ww][:, dim])[0] 
+                pvals[ww -1, dim] = stats.ks_2samp(final_dict[0][:, dim], final_dict[ww][:, dim])[1] 
+
+    def divergence_doc2vec(self):
+        distrs = distributions(data_ref = self.data_ref, data_h0 = self.data_h0, data_h1 = self.data_h1, test = self.test, 
+                        sample_size = self.sample_size, windows = self.windows, drift_type = self.drift_type, 
+                        embedding_model = self.embedding_model, model_name = self.model_name, 
+                        transformation = self.transformation, iterations = self.iters)
+        final_dict = distrs.final_distributions() 
+
+        iterations = len(final_dict)
+        windows = len(final_dict[0])
+        kld = np.zeros((iterations, windows - 1)) # distances or p-values for (ref, every other window)
+        jsd = np.zeros((iterations, windows - 1))
+        for it in range(iterations):
+            for ww in range(1, windows): 
+                kld[it, ww - 1] = self.kl_divergence(final_dict[it][0], final_dict[it][ww]) 
+                jsd[it, ww - 1] = self.js_divergence(final_dict[it][0], final_dict[it][ww])
+
+        # plotting KLD results
+        if self.test == "KL":
+            for window in range(kld.shape[1]):
+                plt.plot(kld[:, window])
+            plt.title("Distances for Doc2Vec + KL Divergence")
+            plt.legend(["ww-"+ str(i) for i in range(kld.shape[1])])
+            plt.show()
+
+        # plotting JSD results
+        if self.test == "JS":
+            for window in range(jsd.shape[1]):
+                plt.plot(jsd[:, window])
+            plt.title("Distances for Doc2Vec + JS Divergence")
+            plt.legend(["ww-"+ str(i) for i in range(jsd.shape[1])])
+            plt.show()
+        return kld, jsd
+
+    def divergence_sbert(self):
+        distrs = distributions(data_ref = self.data_ref, data_h0 = self.data_h0, data_h1 = self.data_h1, test = self.test,
+                        sample_size = self.sample_size, windows = self.windows, drift_type = self.drift_type, 
+                        model_name = self.model_name, embedding_model = self.embedding_model,
+                        transformation = self.transformation, iterations = self.iters)
+        final_dict = distrs.final_distributions() 
+        iterations = len(final_dict.keys())
+        windows = len(final_dict[0].keys())
+        dimensions = final_dict[0][0][0].shape[0]
+        kld = np.zeros((windows - 1, dimensions)) # distances or p-values for (ref, every other window)
+        jsd = np.zeros((windows - 1, dimensions))
+
+        # for it in range(iterations:
+        for ww in range(1, windows): 
+            for dim in range(dimensions):
+                kld[ww -1, dim] = self.kl_divergence(final_dict[0][0][:, dim], final_dict[0][ww][:, dim]) 
+                jsd[ww -1, dim] = self.js_divergence(final_dict[0][0][:, dim], final_dict[0][ww][:, dim])
+
+        # plotting KLD results
+        if self.test == "KL":
+            for window in range(kld.shape[0]):
+                plt.plot(kld[window, :30])
+            plt.title("Distances for SBERT + KL Divergence")
+            plt.legend(["ww-"+ str(i) for i in range(kld.shape[1])])
+            plt.show()
+
+        # plotting JSD results
+        if self.test == "JS":
+            for window in range(jsd.shape[0]):
+                plt.plot(jsd[window, :30])
+            plt.title("Distances for SBERT + JS Divergence")
+            plt.legend(["ww-"+ str(i) for i in range(jsd.shape[1])])
+            plt.show()
+        return kld, jsd
+
+    def detector(self):
+        if self.test == "KS":
+            if self.embedding_model == "Doc2Vec":
+                return self.ks_doc2vec()
+            if self.embedding_model == "SBERT":
+                return self.divergence_sbert()
+
+        elif self.test == "KL":
+            if self.embedding_model == "Doc2Vec":
+                return self.divergence_doc2vec()[0]
+            if self.embedding_model == "SBERT":
+                return self.divergence_sbert()[0]
+
+        elif self.test == "JS":
+            if self.embedding_model == "Doc2Vec":
+                return self.divergence_doc2vec()[1]
+            if self.embedding_model == "SBERT":
+                return self.divergence_sbert()[1]
+      
+        else:
+            print("The specified test or embedding model is not part of the package yet")
