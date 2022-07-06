f3b112e3ab451e8ac8ddac97c7854f6cb57e10dd
akshitasingh0706 <akshitasingh0706@gmail.com>
2022-07-06 16:16:28 -0400

    further cleanup

---
+++ fileImports.py
@@ -0,0 +1,9 @@
+class imports:
+    def run():
+        from sampling import samplingData
+        from baseModels import baseModels
+        from embedding import embedding
+        from distributions import distributions
+        from myDetectors import detectors
+        from AlibiDetectors import AlibiDetectors
+        return samplingData, baseModels, embedding, distributions, detectors, AlibiDetectors
