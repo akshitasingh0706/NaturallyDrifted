cd2d46683edab4adbd7c6059177dfa93de255221
Akshita Singh <37257506+akshitasingh0706@users.noreply.github.com>
2022-08-08 19:18:27 -0400

    cleaning up

--- src/alibiDetectors/allDetectors.py
+++ src/alibiDetectors/allDetectors.py
@@ -22,25 +22,7 @@ from basicDetectors import basicDetectors
 from onlineDetectors import onlineDetectors
 from contextDetectors import contextDetectors
 
-# class allDetectors(basicDetectors, onlineDetectors, contextDetectors, detectorParent):
 class allDetectors(onlineDetectors, contextDetectors, basicDetectors):
-    # def __init__(self, test, drift_type, *args, **kwargs):
-    #     # detectorParent.__init__(self, *args, **kwargs)
-    #     # samplingData.__init__(self, *args, **kwargs)
-    #     # basicDetectors.__init__(self, *args, **kwargs)
-    #     # onlineDetectors.__init__(self, *args, **kwargs)
-    #     # contextDetectors.__init__(self, *args, **kwargs)
-
-    #     self.test = test
-    #     self.drift_type = drift_type
-
-    #     if self.test in ["MMD", "LSDD"] and self.drift_type in ['Sudden', 'Gradual']:
-    #         basicDetectors.__init__(self, *args, **kwargs)
-    #     elif self.test in ["MMD", "LSDD"] and self.drift_type == "Online":
-    #         onlineDetectors.__init__(self, *args, **kwargs)
-    #     elif self.test in ["MMD", "LSDD"] and self.context_type is not None:
-    #         contextDetectors.__init__(self, *args, **kwargs)
-
     def run(self):
         if self.test in ["MMD", "LSDD"] and self.drift_type in ['Sudden', 'Gradual']:
             return basicDetectors.run(self)
